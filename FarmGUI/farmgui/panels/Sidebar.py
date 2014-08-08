ee5329fd6795fe45dc060e519b926fb53de01eae
Alexander Lang <alexander.lang@gmail.com>
2014-08-08 22:07:09 +0200

    small plot improvement, work in progress

--- FarmGUI/farmgui/panels/Sidebar.py
+++ FarmGUI/farmgui/panels/Sidebar.py
@@ -6,10 +6,13 @@ Created on Feb 25, 2014
 
 from pyramid_layout.panel import panel_config
 
+from ..models import DBSession
+from ..models import Parameter
+
 
 @panel_config(name='sidebar', renderer='farmgui:panels/templates/sidebar.pt')
 def sidebar(context, request):
-    def sidebar_item(name, url):
+    def configuration_item(name, url):
         active = request.current_route_url() == url
         item = dict(
             name=name,
@@ -18,12 +21,17 @@ def sidebar(context, request):
         )
         return item
 
-    sidebar_items = []
+    configuration_items = []
+    parameter_items = []
 
     if request.current_route_url().startswith(request.route_url('configuration_views_home')):
         # configuration sidebar
-        sidebar_items.append(sidebar_item('Field Settings', request.route_url('field_settings_list')))
-        sidebar_items.append(sidebar_item('Farm Components', request.route_url('components_list')))
-        sidebar_items.append(sidebar_item('Periphery Controllers', request.route_url('periphery_controllers_list')))
+        configuration_items.append(configuration_item('Field Settings', request.route_url('field_settings_list')))
+        configuration_items.append(configuration_item('Farm Components', request.route_url('components_list')))
+        configuration_items.append(configuration_item('Periphery Controllers', request.route_url('periphery_controllers_list')))
+    if request.current_route_url().startswith(request.route_url('display_views_home')):
+        # display sidebar
+        parameter_items = DBSession.query(Parameter).all()
 
-    return {'sidebar_items': sidebar_items}
+    return {'configuration_items': configuration_items,
+            'parameter_items': parameter_items}
