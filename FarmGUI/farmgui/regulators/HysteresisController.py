d03184526104955535db3cb5b2488c2369b69a25
AlexanderLang <alexander.lang@gmail.com>
2015-02-19 10:27:54 +0100

    added hysteresis controller

---
+++ FarmGUI/farmgui/regulators/HysteresisController.py
@@ -0,0 +1,61 @@
+"""
+Created on Oct 15, 2014
+
+@author: alex
+"""
+
+from farmgui.regulators import Regulator
+from farmgui.regulators import RegulatorProperty
+
+
+class HysteresisController(Regulator):
+
+    def __init__(self):
+        Regulator.__init__(self, 'Two Level Regulator')
+        self.inputs['value'] = RegulatorProperty(0, '')
+        self.inputs['setpoint'] = RegulatorProperty(0, '')
+        self.constants['uh'] = RegulatorProperty(1, 'Upper Hysteresis')
+        self.constants['lh'] = RegulatorProperty(1, 'Lower Hysteresis')
+        self.outputs['inc'] = RegulatorProperty(0, 'Increment output')
+        self.outputs['dec'] = RegulatorProperty(0, 'Decrement output')
+        self.state = 'OK'
+
+    def execute(self, inputs):
+        ret_dict = {}
+        if self.is_executable(inputs):
+            diff = inputs['setpoint'] - inputs['value']
+            uh = self.constants['uh']
+            lh = self.constants['lh']
+            if self.state == 'OK':
+                if diff > -uh:
+                    ret_dict['inc'] = 0
+                    ret_dict['dec'] = 1
+                    self.state = 'dec'
+                elif diff > lh:
+                    ret_dict['inc'] = 1
+                    ret_dict['dec'] = 0
+                    self.state = 'inc'
+                else:
+                    ret_dict['inc'] = 0
+                    ret_dict['dec'] = 0
+                    self.state = 'OK'
+            elif self.state == 'dec':
+                if diff < 0:
+                    ret_dict['inc'] = 0
+                    ret_dict['dec'] = 1
+                else:
+                    ret_dict['inc'] = 0
+                    ret_dict['dec'] = 0
+                    self.state = 'OK'
+            else:
+                if diff > 0:
+                    ret_dict['inc'] = 1
+                    ret_dict['dec'] = 0
+                else:
+                    ret_dict['inc'] = 0
+                    ret_dict['dec'] = 0
+                    self.state = 'OK'
+        else:
+            ret_dict['inc'] = None
+            ret_dict['dec'] = None
+        return ret_dict
--- FarmGUI/farmgui/regulators/__init__.py
+++ FarmGUI/farmgui/regulators/__init__.py
@@ -4,10 +4,30 @@ from .P import P
 from .PI import PI
 from .Difference import Difference
 from .RootHumidifier import RootHumidifier
+from .HysteresisController import HysteresisController
+
+
+class UnknownRegulator(Exception):
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name + ' is not a known regulator class'
 
 
 def regulator_factory(name):
-    return eval(name + "()")
+    if name == 'P':
+        return P()
+    elif name == 'PI':
+        return PI()
+    elif name == 'Difference':
+        return Difference()
+    elif name == 'RootHumidifier':
+        return RootHumidifier()
+    elif name == 'HysteresisController':
+        return HysteresisController()
+    else:
+        raise UnknownRegulator(name)
 
 
 def get_regulator_list():
